5bd2d4a23e36c2c572bd9a9bfea2a3c704b1acd9
milano12345 <dmilano857@gmail.com>
2019-09-05 14:54:42 -0500

    created dashboard component + buttons

---
+++ baseballapp/src/components/Dashboard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Dashboard() {
+
+    return (
+        <div className='buttons'>
+            <button>Strike</button>
+            <button>Ball</button>
+            <button>Fowl</button>
+            <button>Hit</button>
+        </div>
+    )
+}
+
+export default Dashboard;
