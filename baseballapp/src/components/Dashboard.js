a86b7b55f11d8e0fd2a568bcd6cd4207be5fc067
milano12345 <dmilano857@gmail.com>
2019-09-05 18:52:37 -0500

    added logic to display board

--- baseballapp/src/components/Dashboard.js
+++ baseballapp/src/components/Dashboard.js
@@ -21,14 +21,32 @@ class Dashboard extends Component {
   }
 }
 
+IncrementBall = () => {
+  if(this.state.balls < 4){
+  this.setState({ balls: this.state.balls + 1 });
+} else if(this.state.balls === 4) {
+  this.setState({ balls: 0})
+  }
+}
+
+IncrementFowl = () => {
+  if(this.state.strikes < 2){
+  this.setState({ strikes: this.state.strikes + 1, fowls: this.state.fowls + 1 });
+} }
+
+IncrementHit = () => {
+  this.setState({strikes: 0, balls: 0, fowls: 0, hits: this.state.hits + 1})
+}
+
+
   render() {
     return (
         <div className='scoreboard'>
           <Display data={this.state} />
             <button onClick={this.IncrementStrike}>Strike</button>
-            <button>Ball</button>
-            <button>Fowl</button>
-            <button>Hit</button>
+            <button onClick={this.IncrementBall}>Ball</button>
+            <button onClick={this.IncrementFowl}>Fowl</button>
+            <button onClick={this.IncrementHit}>Hit</button>
         </div>
     )
   }
