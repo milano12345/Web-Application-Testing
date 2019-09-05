2c20f58f3ba55af47b8e9af45ba499f0c811d139
milano12345 <dmilano857@gmail.com>
2019-09-05 18:39:36 -0500

    added display component

--- baseballapp/src/components/Dashboard.js
+++ baseballapp/src/components/Dashboard.js
@@ -1,15 +1,36 @@
-import React from 'react';
 
-function Dashboard() {
+import React, { Component } from 'react';
+import Display from './Display'
 
+class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      strikes: 0,
+      balls: 0,
+      fowls: 0,
+      hits: 0
+    };
+  }
+
+  IncrementStrike = () => {
+    if(this.state.strikes < 3){
+    this.setState({ strikes: this.state.strikes + 1 });
+  } else if(this.state.strikes === 3) {
+    this.setState({ strikes: 0})
+  }
+}
+
+  render() {
     return (
-        <div className='buttons'>
-            <button>Strike</button>
+        <div className='scoreboard'>
+          <Display data={this.state} />
+            <button onClick={this.IncrementStrike}>Strike</button>
             <button>Ball</button>
             <button>Fowl</button>
             <button>Hit</button>
         </div>
     )
+  }
 }
-
 export default Dashboard;
--- baseballapp/src/components/Display.js
+++ baseballapp/src/components/Display.js
@@ -1,2 +1,16 @@
 import React from 'react';
 
+const Display = (props) => {
+
+    return (
+        <div>
+        <div>Strikes: {props.data.strikes}</div>
+        <div>Balls: {props.data.balls}</div>
+        <div>Fowls: {props.data.fowls}</div>
+        <div>Hits: {props.data.hits}</div>
+        </div>
+    )
+
+}
+
+export default Display;
